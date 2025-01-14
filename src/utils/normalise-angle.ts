70243299ae75875832a6d6fc72785f9f31861ff4
Ervis <lervis87@gmail.com>
2025-01-14 11:30:45 +0000

    adds precommit hooks

--- src/utils/normalise-angle.ts
+++ src/utils/normalise-angle.ts
@@ -2,7 +2,7 @@ export const normaliseAngle = (angle: number): number => {
   // Calculates remainder to normalise positive angles to the range [0,359].
   const remainderAngle = angle % 360;
 
-  // If remainder is negative, ajust it to the range [0,359] (-90 becomes 270).
+  // If remainder is negative, adjust it to the range [0,359] (-90 becomes 270).
   if (remainderAngle < 0) {
     return remainderAngle + 360;
   }
